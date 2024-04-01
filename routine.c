e9d3f3a8c45baaec2bf9e67af9f5c830d203ac85
gabsanch <130669207+gasanche@users.noreply.github.com>
2024-04-01 16:55:54 +0000

    hemos avanzado muchi

--- routine.c
+++ routine.c
@@ -1,16 +1,53 @@
-void eat()
+void eat(t_philo *philo)
 {
+    pthread_mutex_lock(&philo->left);
+    print_status(philo, "has taken a fork", 0);
+    if (philo->agora->n_phils == 1)
+    {
+      print_status(philo, "died", 1);
+      pthread_mutex_unlock(&philo->left);
+      return ;
+    }
+    pthread_mutex_lock(&philo->right);
+    print_status(philo, "has taken a fork", 0);
+    pthread_mutex_lock(&philo->died);
+    philo->died = time_passed(philo->agora->start) + philo->agora->ttdie;
+    pthread_mutex_unlock(&philo->died);
+    if (philo->lst_eat != 0) //amigo dannyel del pasado, parsea mejor lolololol
+        print_status(philo, "is eating", 0);
+    //hacer una funcion del tiempo de comida
+    //hacer una funcion del tiempo de sueño
+    pthread_mutex_unlock(&philo->left);
+    pthread_mutex_unlock(&philo->right);
+    if (philo->lst_eat != 0) //amigo dannyel del pasado, parsea mejor lolololol
+        print_status(philo, "is sleeping", 0);
+    sleeping(philo->agora->ttsleep, philo->agora);
+    if (philo->lst_eat != 0)
+        print_status(philo, "is thinking", 0);
+    }
 
-}
-
-void sleep()
+void sleeping(int ttsleep, t_agora *agora)
 {
 
 }
 
 int food_supervisor(t_agora *agora)
 {
+    int i;
+    int aux;
 
+    i = 0;
+    pthread_mutex_lock(&agora->end_ph);
+    aux = agora->min_meals;
+    pthread_mutex_unlock(&agora->end_ph);
+    if (aux == agora->n_phils)
+    {
+        pthread_mutex_lock(&agora->end);
+        agora->end = 1;
+        pthread_mutex_unlock(&agora->end);
+        return (1);
+    }
+    return (0);
 }
 
 long int    time_passed(struct timeval start)
@@ -40,7 +77,7 @@ void    sargent(t_agora *agora)
             pthread_mutex_lock(&agora->end);
             agora->end = 1;
             pthread_mutex_unlock(&agora->end);
-            //show status
+            print_status(&agora->philo[i], "died", 1);
             break ;
         }
         if (agora->min_meals > 0 && food_supervisor(agora))
